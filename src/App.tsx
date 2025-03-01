58abfb166158574e247d577f66bd807c7a26ed85
Orestis <ozoidis324@gmail.com>
2025-03-01 15:52:05 +0200

    TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <button className="w-1/6 rounded-full bg-pink-500/75 hover:bg-emerald-500">Save Changes</button>
     </>
   )
 }
